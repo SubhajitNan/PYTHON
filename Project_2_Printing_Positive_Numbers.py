e01f388a106cbe5a590f0a21073032d95e65f04a
Subhajit Nan <86914943+SubhajitNan@users.noreply.github.com>
2021-08-03 22:45:59 +0530

    Update Project_2_Printing_Positive_Numbers.py

--- Project_2_Printing_Positive_Numbers.py
+++ Project_2_Printing_Positive_Numbers.py
@@ -2,7 +2,6 @@
  
 # list of numbers
 list1 = [12, -7, 5, 64, -14]
-list2 = [12, 14, -95, 3] 
  
 # iterating each number in the first list
 for num in list1:
@@ -11,15 +10,6 @@ for num in list1:
     if num >= 0:
        print(num, end = " ") 
     
-print("\n")
-       
-       
-# iterating each number in the second list
-for num in list2:
-     
-    # checking condition
-    if num >= 0:
-       print(num, end = " ")
         
  ******************************************************************
  
