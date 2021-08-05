a6c5313a706bba618f68e70b5d62681a8b4b9881
Subhajit Nan <86914943+SubhajitNan@users.noreply.github.com>
2021-08-05 23:50:13 +0530

    Update Project_2_Printing_Positive_Numbers.py
    Did this code in three different ways.

--- Project_2_Printing_Positive_Numbers.py
+++ Project_2_Printing_Positive_Numbers.py
@@ -1,4 +1,4 @@
-# Python program to print Positive Numbers in a List
+# Python program to print Positive Numbers in a List Using List Comprehension
 
 # list of numbers
 list1 = [-10, -21, -4, 45, -66, 93]
@@ -10,8 +10,34 @@ print("Positive numbers in the list: ", *pos_nos)
       
 ******************************************************************
  
-Output:-
- 
-45 93
+# Python program to print positive Numbers in a List Using While Loop
 
-********************************************************************
+# list of numbers
+list1 = [-10, 21, -4, -45, -66, 93]
+num = 0
+
+# using while loop	
+while(num < len(list1)):
+	
+	# checking condition
+	if list1[num] >= 0:
+		print(list1[num], end = " ")
+	
+	# increment num
+	num += 1
+      
+******************************************************************
+
+# Python program to print positive Numbers in a List
+
+# list of numbers
+list1 = [11, -21, 0, 45, 66, -93]
+
+# iterating each number in list
+for num in list1:
+    if num >= 0:
+
+        # checking condition
+        print(num, end = " ")
+
+*******************************************************************
